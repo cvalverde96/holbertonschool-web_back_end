64a09bc56c302f7c9641b908b29b069cd2f64ab8
cvalverde96 <cvalverde96@gmail.com>
2024-11-11 18:47:35 -0400

    subiendo 10 con eslint compliance

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,7 @@
-export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => `${appendString}${value}`);
+export default function appentToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  return newArray;
 }
