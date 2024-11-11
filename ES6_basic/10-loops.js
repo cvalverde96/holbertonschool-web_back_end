a4600efa2adaf87974ef1c548bfbf73d08af3c0f
cvalverde96 <cvalverde96@gmail.com>
2024-11-11 18:42:59 -0400

    subiendo el 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
-/*eslint-disable*/
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [index, value] of array.entries()) {
-    array[index] = appendString + value;
-  }
-  return array;
+  return array.map((value) => `${appendString}${value}`);
 }
