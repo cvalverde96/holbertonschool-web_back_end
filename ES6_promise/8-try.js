b6c617f6abd76f86ac023a94247429fca96d74d3
cvalverde96 <cvalverde96@gmail.com>
2024-11-20 11:40:57 -0400

    subiendo el 8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,9 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  return Promise.try(() => {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    } else {
-      return numerator / denominator;
-    }
-  });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
 }
