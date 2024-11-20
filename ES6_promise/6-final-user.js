1ac1afe2c0c9c581c324e740a9381a411532c242
cvalverde96 <cvalverde96@gmail.com>
2024-11-20 10:02:40 -0400

    subiendo el 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled([
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
-  return results.map((result) => ({
+  ]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason.message,
-  }));
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  })));
 }
