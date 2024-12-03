f1431afb721ea3bb7cde70407dc594a07e59512f
cvalverde96 <cvalverde96@gmail.com>
2024-12-03 13:49:32 -0400

    subiendo 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from typing import List
+
+"""Module that contains a function that takes a list of floats and sums them"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """_summary_
+
+    Args:
+        input_list (List[float]): list of floats to be summed
+
+    Returns:
+        float: sum of floats
+    """
+    return sum(input_list)
