3e08632028724a1d17fbbf3280cdd010a108b8e5
cvalverde96 <cvalverde96@gmail.com>
2024-12-03 13:18:08 -0400

    subiendo ejercicio 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+concat two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): first string to concat
+        str2 (str): second string to concat
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
