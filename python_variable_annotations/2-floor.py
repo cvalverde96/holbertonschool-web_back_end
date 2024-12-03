e3302b6c231f727e65f7b5d442db35cd4436598d
cvalverde96 <cvalverde96@gmail.com>
2024-12-03 13:22:31 -0400

    subiendo ejercicio 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import math
+
+"""
+Module to return fllor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float): float to be floored
+
+    Returns:
+        int: the floored value of the float
+    """
+    return math.floor(n)
