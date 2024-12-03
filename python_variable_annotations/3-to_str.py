99790de2af30e23a1f2d312b2354364b13f69720
cvalverde96 <cvalverde96@gmail.com>
2024-12-03 13:38:54 -0400

    subiendo el 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that converts a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    Args:
+        n (float): float to convert to string
+
+    Returns:
+        str: string conversion of float
+    """
+    return str(n)
