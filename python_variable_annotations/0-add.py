fa903f5435f78c1408b5a27f1dfe88b616a86b60
cvalverde96 <cvalverde96@gmail.com>
2024-12-03 13:13:16 -0400

    subiendo 0 con documentacion mas extensa

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,4 +6,13 @@ Module that adds two floats with type annotations
 
 
 def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): first number to add
+        b (float): second number to add
+
+    Returns:
+        float: the sum of both numbers
+    """
     return a + b
