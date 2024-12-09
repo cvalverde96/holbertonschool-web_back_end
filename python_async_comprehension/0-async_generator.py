e513abbcf57798c97bd8b478aca96ec66382a783
cvalverde96 <cvalverde96@gmail.com>
2024-12-09 11:21:21 -0400

    subiendo 0 con loop que mpieze desde 0 y no desde 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -23,5 +23,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
         returns a random number that is float
     """
     for _ in range(10):
-        yield random.uniform(1.0, 10.0)
+        yield random.uniform(0.0, 10.0)
         await asyncio.sleep(1)
