974fd1a08b1d0a0caffbed10b3cea0137dedd40a
cvalverde96 <cvalverde96@gmail.com>
2024-12-09 12:58:52 -0400

    subiendo ejercicio con dos none en vez de uno

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,12 +7,11 @@ then yield a random number between 0 and 10.
 Using the random module.
 """
 import asyncio
-from asyncio import sleep
 import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """_summary_
 
     Returns:
