7d7df7a1298367056877b9e3bc833215187ba416
cvalverde96 <cvalverde96@gmail.com>
2024-12-12 12:39:24 -0400

    subiendo el 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 
 class Server:
@@ -39,7 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: Optional[int] = None, page_size: int = 10) -> Dict:
         if index is None:
             index = 0
 
