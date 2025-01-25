dce120cedf15878926bb2635e88b1eda405c43f2
cvalverde96 <cvalverde96@gmail.com>
2025-01-25 11:23:42 -0400

    subiendo 5 arreglando

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,22 +4,24 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  if (req.url === '/') {
+  const { url } = req;
+  const path = process.argv[2];
+
+  if (url === '/') {
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    const databasePath = process.argv[2];
-    let responseText = 'This is the list of our students\n';
-
-    try {
-      const data = await countStudents(databasePath);
-      const studentCounts = Object.entries(data)
-        .map(([field, fieldData]) => `Number of students in ${field}: ${fieldData.count}. List: ${fieldData.names.join(', ')}`).join('\n');
-      responseText += studentCounts;
-    } catch (error) {
-      responseText += 'Cannot load the database';
+  } else if (url === '/students') {
+    if (path !== null) {
+      const message = 'This is the list of our students\n';
+      try {
+        const students = await countStudents(path);
+        res.end(`${message}${students.join('\n')}`);
+      } catch (err) {
+        res.end(`${message}${err.message}`);
+      }
     }
-
-    res.end(responseText);
+  } else {
+    res.write('Not Found');
+    res.end();
   }
 });
 
