371382e53d17826a99762f48eddb0850b2c1f2ba
cvalverde96 <cvalverde96@gmail.com>
2025-01-25 10:39:24 -0400

    subiendo el 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const fileContent = await fs.readFile(path, 'utf-8');
+    const lines = fileContent.trim().split('\n');
+    const students = lines.sclice(1).filter((line) => line.length > 0);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+      if (!fields[fields]) {
+        fields[field] = { count: 0, names: [] };
+      }
+      fields[field].count += 1;
+      fields[field].names.push(firstname);
+    });
+
+    Object.entries(fields).forEach(([field, data]) => {
+      const nameList = data.names.join(', ');
+      console.log(`Number of students in ${field}: ${data.count}. List: ${nameList}`);
+    });
+    return fields;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
